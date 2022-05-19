e9599d6aa3557d1f2e0614538e55cc2ac35a7888
woonys <wodns1324@naver.com>
2022-05-20 03:08:04 +0900

    DoWhile

---
+++ ThisIsJava_/src/Ch4/DoWhileEx.java
@@ -0,0 +1,22 @@
+package Ch4;
+
+import java.util.Scanner;
+
+public class DoWhileEx {
+    public static void main(String[] args) {
+        System.out.println("메시지를 입력하세요");
+        System.out.println("프로그램을 종료하려면 q를 입력하세요.");
+
+        Scanner scanner = new Scanner(System.in); // 객체 생성
+        String inputString;
+
+        do {
+            System.out.print(">");
+            inputString = scanner.nextLine(); // 키보드로 입력한 문자열 얻는다. 한 라인 통으로.
+            System.out.println(inputString);
+        } while (!inputString.equals("q")); // q가 안 나오면 while 문 계속 돈다 -> do 계속 실행
+
+        System.out.println();
+        System.out.println("프로그램 종료");
+    }
+}
