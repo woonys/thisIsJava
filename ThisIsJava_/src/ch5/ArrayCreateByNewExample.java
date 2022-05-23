96261168e7128b4c2595c4f8e7cd1db62b3c7718
woonys <wodns1324@naver.com>
2022-05-23 18:59:11 +0900

    TvTest2

--- ThisIsJava_/src/ch5/ArrayCreateByNewExample.java
+++ ThisIsJava_/src/ch5/ArrayCreateByNewExample.java
@@ -1,12 +1,10 @@
-package ch5;
-
-import java.sql.SQLOutput;
+package Ch5;
 
 public class ArrayCreateByNewExample {
     public static void main(String[] args) {
         int[] arr1 = new int[3];
         for (int i=0; i<3; i++) {
-            System.out.println("arr1[" + i + "] :" + arr1[i]);
+            System.out.println("arr1[" + i + "] :" + arr1[i]); // int 배열 초기값은 0
         }
         arr1[0] = 10;
         arr1[1] = 20;
@@ -17,7 +15,7 @@ public class ArrayCreateByNewExample {
 
         double[] arr2 = new double[3];
         for (int i=0; i<3; i++) {
-            System.out.println("arr2[" + i + "] : " + arr2[i]);
+            System.out.println("arr2[" + i + "] : " + arr2[i]); // double 배열 초기값은 0.0
         }
         arr2[0] = 0.1;
         arr2[1] = 0.2;
