4cddcb4895b69e6bd2b94d82d89ec1aad47a42c0
woonys <wodns1324@naver.com>
2022-05-24 14:45:11 +0900

    ch6-ex09

---
+++ ThisIsJava_/src/Ch6/Ex09.java
@@ -0,0 +1,61 @@
+package Ch6;
+
+import java.util.Scanner;
+
+public class Ex09 {
+    public static void main(String[] args) {
+        boolean run = true;
+        int studentNum = 0;
+        int[] scores = null;
+        Scanner scanner = new Scanner(System.in);
+
+        while (run) {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("1. 학생 수 | 2. 점수 입력 | 3. 점수 리스트 | 4. 분석 | 5. 종료");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("선택> ");
+
+            int selectNo = scanner.nextInt();
+
+            if (selectNo == 1) {
+                System.out.println("학생 수> ");
+                studentNum = scanner.nextInt();
+                if (studentNum <= 0) {
+                    System.out.println("학생 수는 0명 이하가 될 수 없습니다.");
+                } else {
+                    scores = new int[studentNum];
+                }
+            } else if (selectNo == 2) {
+                for (int i=0; i < scores.length; i++) {
+                    System.out.println("scores[" + i + "]");
+                    int studentScore = scanner.nextInt();
+                    scores[i] = studentScore;
+                }
+            } else if (selectNo == 3) {
+                for (int i=0; i < scores.length; i++) {
+                    System.out.println("scores[" + i + "] : " + scores[i]);
+                }
+            } else if (selectNo == 4) {
+                int max = 0;
+                int sum = 0;
+                for (int i=0; i< scores.length; i++) {
+                    if (max < scores[i]) {
+                        max = scores[i];
+                    }
+                }
+
+                for (int i=0; i< scores.length; i++) {
+                     sum += scores[i];
+                    }
+                double avg = sum / scores.length;
+
+                System.out.println("최고 점수: " + max);
+                System.out.println("평균 점수: " + avg);
+            } else if (selectNo == 5) {
+                run = false;
+            }
+        }
+
+        System.out.println("프로그램 종료");
+    }
+}
