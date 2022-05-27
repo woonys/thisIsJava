228e2a325f82cb004654490b0567c0514afbd07c
woonys <wodns1324@naver.com>
2022-05-27 21:21:30 +0900

    Ch06-EarthExercise

---
+++ ThisIsJava_/src/Ch6/Calculator.java
@@ -0,0 +1,21 @@
+package Ch6;
+
+public class Calculator {
+    // 메소드
+    void powerOn() {
+        System.out.println("전원을 켭니다.");
+    }
+    int plus(int x, int y) {
+        int result  = x + y;
+        return result;
+    }
+
+    double divide(int x, int y) {
+        double result  = (double) x / (double) y; // int를 double로 타입 캐스팅
+        return result;
+    }
+
+    void powerOff() {
+        System.out.println("전원을 끕니다.");
+    }
+}
---
+++ ThisIsJava_/src/Ch6/Earth.java
@@ -0,0 +1,11 @@
+package Ch6;
+
+public class Earth {
+    // 상수 선언
+    static final double EARTH_RADIUS = 6400;
+    static final double EARTH_SURFACE_AREA;
+
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+    }
+}
