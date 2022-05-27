947b394b2e1819e12fe9be75d97d1746f0fdf21a
woonys <wodns1324@naver.com>
2022-05-28 01:50:18 +0900

    Ch6-ex14

---
+++ ThisIsJava_/src/Ch6/Exercise/Ex14_MemberwithConstructor.java
@@ -0,0 +1,15 @@
+package Ch6.Exercise;
+
+public class Ex14_MemberwithConstructor {
+    // 필드로만 구성된 클래스
+    String name;
+    String id;
+    String password;
+    int age;
+
+    // 생성자 선언
+    Ex14_MemberwithConstructor(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
