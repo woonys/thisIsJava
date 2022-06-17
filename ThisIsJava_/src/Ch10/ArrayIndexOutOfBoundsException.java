09a1e4087f9c5e68884c049c14dc94a562be9b15
woonys <wodns1324@naver.com>
2022-06-17 17:33:41 +0900

    Ch10-ArrayIndexOutOfBoundsException_refactoring

--- ThisIsJava_/src/Ch10/ArrayIndexOutOfBoundsException.java
+++ ThisIsJava_/src/Ch10/ArrayIndexOutOfBoundsException.java
@@ -2,11 +2,22 @@ package Ch10;
 
 public class ArrayIndexOutOfBoundsException {
     public static void main(String[] args) {
-        String data1 = args[0];
-        String data2 = args[1];
+        // main()에 들어가는 인자 args에는 어떤 실행 매개값도 주어지지 않았음.
 
-        System.out.println("args[0]: " + data1);
-        System.out.println("args[1]: " + data2);
+        // 예제 수정
+        if(args.length == 2) {
+            String data1 = args[0];
+            String data2 = args[1];
+
+            System.out.println("args[0]: " + data1);
+            System.out.println("args[1]: " + data2);
+            return;
+        }
+        System.out.println("[실행 방법]");
+        System.out.println("java ArrayIndexOutOfBoundsExceptionExample ");
+        System.out.println("값1 값2");
+
+
     }
 }
 
