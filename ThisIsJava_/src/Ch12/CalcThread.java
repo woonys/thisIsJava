8a6be30f8b70c97f5d9ba7a997c674265c7d5cb1
woonys <wodns1324@naver.com>
2022-06-28 22:56:59 +0900

    Ch12-ThreadPriorityExample

---
+++ ThisIsJava_/src/Ch12/CalcThread.java
@@ -0,0 +1,13 @@
+package Ch12;
+
+public class CalcThread extends Thread{
+    public CalcThread(String name) {
+        setName(name); // 스레드 이름 변경
+    }
+
+    public void run() { // 스레드가 실행할 내용
+        for (int i=0; i < 2_000_000_000; i++) {
+        }
+        System.out.println(getName());
+    }
+}
---
+++ ThisIsJava_/src/Ch12/PriorityExample.java
@@ -0,0 +1,15 @@
+package Ch12;
+
+public class PriorityExample {
+    public static void main(String[] args) {
+        for (int i=1; i <= 10; i++) {
+            Thread thread = new CalcThread("thread" + i); // 괄호 안은 스레드 이름
+            if (i != 10) {
+                thread.setPriority(Thread.MIN_PRIORITY); // 가장 낮은 우선순위 설정
+            } else {
+                thread.setPriority(Thread.MAX_PRIORITY); // 가장 높은 우선순위 설정
+            }
+            thread.start();
+        }
+    }
+}
