4006b661e6f4b308a7bbb81ad4d8e2bfe187e1fc
woonys <wodns1324@naver.com>
2022-07-11 22:54:15 +0900

    Ch13-BoundedTypeParameterExample

--- ThisIsJava_/src/Ch13/Util.java
+++ ThisIsJava_/src/Ch13/Util.java
@@ -1,5 +1,7 @@
 package Ch13;
 
+import javafx.beans.binding.NumberBinding;
+
 public class Util {
     public static <T> Box<T> boxing(T t) {
         Box<T> box = new Box<>();
@@ -11,5 +13,11 @@ public class Util {
         boolean keyCompare = p1.getKey().equals(p2.getKey());
         boolean valueCompare = p1.getValue().equals(p2.getValue());
         return keyCompare && valueCompare;
+    }
+
+    public static <T extends Number> int compare(T t1, T t2) {
+        double v1 = t1.doubleValue();
+        double v2 = t2.doubleValue();
+        return Double.compare(v1, v2);
     }
 }
