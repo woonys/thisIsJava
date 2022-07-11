8cf48c0f575a727a369c57088ea276c5dc5c1ba5
woonys <wodns1324@naver.com>
2022-07-11 22:11:31 +0900

    Ch13-Multitype Generic

---
+++ ThisIsJava_/src/Ch13/Car.java
@@ -0,0 +1,7 @@
+package Ch13;
+
+public class Car {
+    public String model;
+
+}
+
---
+++ ThisIsJava_/src/Ch13/ProductExample.java
@@ -0,0 +1,21 @@
+package Ch13;
+
+
+public class ProductExample {
+    public static void main(String[] args) {
+        Product<Tv, String> product1 = new Product<Tv, String>();
+        product1.setKind(new Tv());
+        product1.setModel("스마트 Tv");
+        Tv tv = product1.getKind();
+        String tvModel = product1.getModel();
+
+        Product<Car, String> product2 = new Product<Car, String>();
+        product2.setKind(new Car());
+        product2.setModel("디젤");
+        Car car = product2.getKind();
+        String carModel = product2.getModel();
+
+        System.out.println(tvModel);
+        System.out.println(carModel);
+    }
+}
