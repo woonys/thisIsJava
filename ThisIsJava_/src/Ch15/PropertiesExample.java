b448848f06481e183f0b7785f3df3ff0025c2ad7
woonys <wodns1324@naver.com>
2022-08-23 00:00:01 +0900

    15.4.3 Properties

---
+++ ThisIsJava_/src/Ch15/PropertiesExample.java
@@ -0,0 +1,12 @@
+package Ch15;
+
+import java.util.Properties;
+
+import Ch13.Product;
+
+public class PropertiesExample {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        String path = PropertiesExample.class.getResource()
+    }
+}
