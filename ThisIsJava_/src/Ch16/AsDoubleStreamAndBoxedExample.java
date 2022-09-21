b177298659dd4228153b20423af23b3e25d6957a
woonys <wodns1324@naver.com>
2022-09-21 23:02:33 +0900

    Ch16.5 Mapping Example

---
+++ ThisIsJava_/src/Ch16/AsDoubleStreamAndBoxedExample.java
@@ -0,0 +1,23 @@
+package Ch16;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class AsDoubleStreamAndBoxedExample {
+    public static void main(String[] args) {
+        int[] intArray = { 1, 2, 3, 4, 5};
+
+        IntStream intStream = Arrays.stream(intArray);
+        intStream
+            .asDoubleStream()
+            .forEach(System.out::println);
+
+        System.out.println();
+
+        intStream = Arrays.stream(intArray);
+        intStream
+            .boxed()
+            .forEach(System.out::println);
+    }
+}
